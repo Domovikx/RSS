be54f83fb4593d8bc92107318c467c7d6f552a87
Ilya Ivanouski <domovikx@gmail.com>
2019-06-22 22:10:38 +0300

    Revert "fix issues #1-8"
    This reverts commit bb3fa258e65e45643997cf9b5453047280ff5c26.

--- examples/animation shape change/animation-shape-change.js
+++ examples/animation shape change/animation-shape-change.js
@@ -2,18 +2,24 @@ function shapeChange() {
     const COLOR_RED = 'rgb(255, 0, 0)';
     const COLOR_GREEN = 'rgb(0, 255, 0)';
 
-    const shape = document.getElementById('shape');
-    const computedStyle = window.getComputedStyle(shape);
-
+    let shape = document.querySelector("#shape");
+    let compStyles = window.getComputedStyle(shape);
+    let boundingClientRect = shape.getBoundingClientRect();
     let count = 0;
 
     console.log('shape :', shape); // элемент
-    console.log('computedStyle', computedStyle); // выводим все стили элемента
+    console.log('compStyles', compStyles); // выводим все стили элемента
+    console.log('boundingClientRect :', boundingClientRect); // размеры и положение элемента
+
+    setInterval(() => {
+        animation();
+        countInterval();
+    }, 1500); // задаем интервал анимации 
 
     function animation() {
-        const backgroundColor = computedStyle.getPropertyValue('background-color');
+        let backgroundColor = compStyles.getPropertyValue('background-color');
 
-        if (backgroundColor === COLOR_RED) {
+        if (backgroundColor == COLOR_RED) {
             shape.style.backgroundColor = COLOR_GREEN;
             shape.style.height = '100px';
             shape.style.width = '100px';
@@ -28,15 +34,9 @@ function shapeChange() {
         }
     }
 
-    function increaseCount() {
+    function countInterval() {
         count++;
         document.getElementById("count").innerHTML = count;
     }
-
-    setInterval(() => {
-        animation();
-        increaseCount();
-    }, 1500); // задаем интервал анимации 
-
 }
 shapeChange();
