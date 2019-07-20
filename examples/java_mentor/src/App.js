58aa644cd13a3d4b64822bd9df1cc98137f15ec3
Ilya Ivanouski <domovikx@gmail.com>
2019-07-20 16:14:54 +0300

    add examples

--- examples/java_mentor/src/App.js
+++ examples/java_mentor/src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => (<div><ProgrammingLanguageRating /></div>)
+export default App;
+
+
+class ProgrammingLanguageRating extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'frontend',
+      url: 'https://frontend-test-api.alex93.now.sh/api/languages?group=',
+      urlRequest: 'https://frontend-test-api.alex93.now.sh/api/languages?group=frontend'
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      value: event.target.value,
+      urlRequest: this.state.url + event.target.value
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    getRequest(this.state.urlRequest);
+    console.log('urlRequest', this.state.urlRequest)
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Programming Language Rating: {this.state.value}
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="backend">Back-end</option>
+            <option value="frontend">Front-end</option>
+            <option value="mobile">Mobile</option>
+          </select>
+
+        </label>
+        <input type="submit" value="Выбрать" />
+        {this.state.urlRequest}
+      </form>
+    );
+  }
 }
 
-export default App;
+const getRequest = async (url) => {
+  const api_url = await fetch(url);
+  const data = await api_url.json();
+  console.log('data', data);
+  return data;
+}
