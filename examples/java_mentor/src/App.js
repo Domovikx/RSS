e38cd8eb45c2fe707db8b0086a7bc6100e93f383
Ilya Ivanouski <domovikx@gmail.com>
2019-07-20 19:16:53 +0300

    feat: add very much

--- examples/java_mentor/src/App.js
+++ examples/java_mentor/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 const App = () => (<div><ProgrammingLanguageRating /></div>)
 export default App;
 
@@ -10,8 +11,9 @@ class ProgrammingLanguageRating extends Component {
     super(props);
     this.state = {
       value: 'frontend',
+      languageRating: [],
       url: 'https://frontend-test-api.alex93.now.sh/api/languages?group=',
-      urlRequest: 'https://frontend-test-api.alex93.now.sh/api/languages?group=frontend'
+      urlRespons: 'https://frontend-test-api.alex93.now.sh/api/languages?group=frontend'
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -21,38 +23,56 @@ class ProgrammingLanguageRating extends Component {
   handleChange(event) {
     this.setState({
       value: event.target.value,
-      urlRequest: this.state.url + event.target.value
+      urlRespons: this.state.url + event.target.value
     })
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    getRequest(this.state.urlRequest);
-    console.log('urlRequest', this.state.urlRequest)
+
+    const getRequest = async () => {
+      const api_url = await fetch(this.state.urlRespons);
+      const languageRating = await api_url.json();
+      await this.setState({ languageRating })
+    }
+    getRequest();
   }
 
   render() {
+    const { languageRating } = this.state;
+    console.log('languageRating', languageRating)
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Programming Language Rating: {this.state.value}
-          <select value={this.state.value} onChange={this.handleChange}>
-            <option value="backend">Back-end</option>
-            <option value="frontend">Front-end</option>
-            <option value="mobile">Mobile</option>
-          </select>
-
-        </label>
-        <input type="submit" value="Выбрать" />
-        {this.state.urlRequest}
-      </form>
+      <>
+        <form className='form-group' onSubmit={this.handleSubmit}>
+          <label for="exampleFormControlSelect1">
+            <p>Programming Language Rating</p>
+            <select class="form-control" id="exampleFormControlSelect1"
+              value={this.state.value} onChange={this.handleChange}>
+              <option value="backend">Back-end</option>
+              <option value="frontend">Front-end</option>
+              <option value="mobile">Mobile</option>
+            </select>
+          </label>
+          <input type="submit" value="Выбрать" />
+        </form>
+        <div>
+          {languageRating.data &&
+            <div>
+              {
+                languageRating.data.map((data, id) =>
+                  <div key={id}>
+                    {data.logo &&
+                      <img src={data.logo} alt="альтернативный текст" width='100px' />}
+                    <p>{data.name}</p>
+                    <p>{data.year}</p>
+                    {data.docs &&
+                      <a href={data.docs}>Документация</a>}
+                  </div>)
+              }
+            </div>
+          }
+        </div>
+      </>
     );
   }
-}
-
-const getRequest = async (url) => {
-  const api_url = await fetch(url);
-  const data = await api_url.json();
-  console.log('data', data);
-  return data;
 }
