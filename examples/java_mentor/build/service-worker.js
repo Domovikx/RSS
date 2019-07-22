613cc0b0087bb745ab9dfd1dbc340ffa7804adfa
Ilya Ivanouski <domovikx@gmail.com>
2019-07-22 22:52:03 +0300

    try again

--- examples/java_mentor/build/service-worker.js
+++ examples/java_mentor/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/RSS/java_mentor/precache-manifest.7ec69d9ab896af6335ffb723c077ed08.js"
+  "/RSS/precache-manifest.81aa7500fca34570cadd648f7369a174.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RSS/java_mentor/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RSS/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
